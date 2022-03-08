4be50f8daed635465ef588d4b2182a4fe5f84042
unknown <Wilson@LAB.COM.BR>
2022-03-08 20:35:20 -0300

    Terceiro commit

---
+++ Entities/Empresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_csharp.Entities
+{
+    /// <summary>
+    /// Classe de entidade
+    /// </summary>
+    public class Empresa
+    {
+        public Guid Id { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+
+    }
+}
