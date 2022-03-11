962259e8beeb50f6d66eab20b28f3e65897bae20
unknown <Wilson@LAB.COM.BR>
2022-03-11 19:13:33 -0300

    Terceiro commit 4

---
+++ Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_csharp.Interfaces
+{
+    /// <summary>
+    /// Interface padrão para definir os métodos dos repositorios
+    /// </summary>
+    public interface IBaseRepository
+    {
+
+    }
+}
