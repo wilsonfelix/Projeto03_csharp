229904ebe7f8cd4046bbd1d59f8e668598436e4c
unknown <Wilson@LAB.COM.BR>
2022-03-11 18:00:43 -0300

    Terceiro commit 2

---
+++ Repositories/FuncionarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_csharp.Repositories
+{
+    /// <summary>
+    /// Classe de repositorio de dados para a entidade Funcionario
+    /// </summary>
+    public class FuncionarioRepository
+    {
+
+    }
+}
