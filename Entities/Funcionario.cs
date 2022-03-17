f2092230d40546889194748cd03c5656d3e532ba
unknown <Wilson@LAB.COM.BR>
2022-03-17 19:45:54 -0300

    Atualização diária 17/03/2022

--- Entities/Funcionario.cs
+++ Entities/Funcionario.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Projeto03_csharp.Entities
 {
-    internal class Funcionario
+    public class Funcionario
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public Guid EmpresaId { get; set; }
     }
 }
