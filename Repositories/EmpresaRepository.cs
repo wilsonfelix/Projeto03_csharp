319f11631dd4f348c4ccdec9ebca90e275793d3e
unknown <Wilson@LAB.COM.BR>
2022-03-17 23:52:22 -0300

    Atualização diária 17/03/2022 - 2

--- Repositories/EmpresaRepository.cs
+++ Repositories/EmpresaRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Projeto03_csharp.Interfaces;
+using Projeto03_csharp.Entities;
+using System.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,71 @@ using System.Threading.Tasks;
 
 namespace Projeto03_csharp.Repositories
 {
-    internal class EmpresaRepository
+    public class EmpresaRepository : IBaseRepository<Empresa>
     {
+        //atributo
+        private readonly string _connectionString = @"Data Source=SRVSQLSERVER;Initial Catalog=SQLLAB1;Integrated Security=True";
+        public void Create(Empresa obj)
+        {
+            //escreve o comando SQL
+            var sqlInsert = @"
+                            INSERT INTO  EMPRESA(ID, RAZAOSOCIAL, NOMEFANTASIA, CNPJ)
+                            VALUES(@Id, @RazaoSocial, @NomeFantasia, @Cnpj)
+                            ";
+
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlInsert, obj);
+            }
+        }
+
+        public void Delete(Empresa obj)
+        {
+            //escreve o comando SQL
+            var sqlDelete = @"
+                            DELETE FROM EMPRESA 
+                            WHERE ID = @Id
+                            ";
+
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlDelete, obj);
+            }
+        }
+
+        public List<Empresa> GetAll()
+        {
+            //escreve o comando SQL
+            var sqlSelect = @"
+                            SELECT * FROM EMPRESA
+                            WHERE ID = @Id
+                            ORDER BY NOMEFANTASIA ASC
+                            ";
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper e retornar o resultado
+                return connection.Query<Empresa>(sqlSelect).ToList();
+            }
+        }
+
+        public void Update(Empresa obj)
+        {
+            //escreve o comando SQL
+            var sqlUpdate = @"
+                            UPDATE EMPRESA SET RAZAOSOCIAL = @RazaoSocial, NOMEFANTASIA = @NomeFantasia, CNPJ = @Cnpj
+                            WHERE ID = @Id
+                            ";
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlUpdate, obj);
+            }
+        }
     }
 }
--- Repositories/FuncionarioRepository.cs
+++ Repositories/FuncionarioRepository.cs
@@ -1,16 +1,88 @@
-﻿using System;
+﻿using Projeto03_csharp.Interfaces;
+using Projeto03_csharp.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Projeto03_csharp.Repositories
 {
     /// <summary>
     /// Classe de repositorio de dados para a entidade Funcionario
     /// </summary>
-    public class FuncionarioRepository
+    public class FuncionarioRepository : IBaseRepository<FuncionarioRepository>
     {
+        private readonly string _connectionString = @"Data Source=SRVSQLSERVER;Initial Catalog=SQLLAB1;Integrated Security=True";
+        public void Create(FuncionarioRepository obj)
+        {
+            //escreve o comando SQL
+            var sqlInsert = @"
+                            INSERT INTO  FUNCIONARIO (ID, NOME, MATRICULA, CPF, DATAADMISSAO, EMPRESAID)
+                            VALUES(@Id, @Nome, @Matricula, @Cpf, @DataAdmissao, @EmpresaId)
+                            ";
 
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlInsert, obj);
+            }
+        }
+
+        public void Delete(FuncionarioRepository obj)
+        {
+            //escreve o comando SQL
+            var sqlDelete = @"
+                            DELETE FROM FUNCIONARIO
+                            WHERE ID = @Id
+                            ";
+
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlDelete, obj);
+            }
+        }
+
+        public List<Funcionario> GetAll()
+        {
+            //escreve o comando SQL
+            var sqlSelect = @"
+                            SELECT * FROM FUNCIONARIO
+                            WHERE ID = @Id
+                            ORDER BY NOME ASC
+                            ";
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper e retornar o resultado
+                return connection.Query<Funcionario>(sqlSelect).ToList();
+            }
+        }
+
+        public void Update(FuncionarioRepository obj)
+        {
+            //escreve o comando SQL
+            var sqlUpdate = @"
+                            UPDATE FUNCIONARIO SET ID = @Id, NOME = @Nome, MATRICULA = @Matricula, CPF = @Cpf, DATAADMISSAO = @DataAdmissao, EMPRESAID = @EmpresaId
+                            WHERE ID = @Id
+                            ";
+
+            //abrindo conexão com o sqlserver
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //executando o comando com Dapper
+                connection.Execute(sqlUpdate, obj);
+            }
+        }
+
+        List<FuncionarioRepository> IBaseRepository<FuncionarioRepository>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
